735d072fc043178ac6e8d3e65ae9884092695f72
Dmitry Dmitrievich Pechurkin <dpechurkin@gmail.com>
2018-01-12 23:45:27 +0500

    first stage

---
+++ index.php
@@ -0,0 +1,16 @@
+﻿
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LMS - Landing manager system </title>
+	<meta name="author" value="Dmitry Dmitrievich Pechurkin">
+	<meta charset="utf-8">
+	<meta name="date" value="12.01.2018">
+</head>
+<body>
+	
+<?
+	
+?>
+</body>
+</html>
